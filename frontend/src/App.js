f7932a2d60edc6ef3953fb2489ac984d0c0b7252
jayantdahiya <jayantdahiya1999@gmail.com>
2022-06-05 22:35:54 +0530

    Created navbar and post button on the home layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './Components/Nav';
+import { Fab } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+    <CssBaseline>
+    <div className='navBar'>
+      <Nav />
+     </div>
+     <div className='postButton'>
+     <Fab color='primary'>
+      <AddIcon/>
+     </Fab>
+     </div>
+    </CssBaseline>
+    </>
   );
 }
 
