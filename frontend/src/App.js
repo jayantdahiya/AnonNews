8062775e4a3accec24af9e8bb60fbeae7f1d8655
jayantdahiya <jayantdahiya1999@gmail.com>
2022-06-16 15:27:06 +0530

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import AnonNews from './AnonNews';
-import {MoralisProvider} from 'react-moralis';
-
-const appId = 'd0cX7Z2B2dveq9LkyGFPSAvn6ap38A853D0nnUmD';
-const serverUrl = 'https://d0aulqajsczr.usemoralis.com:2053/server';
 
 function App() {
   return (
-    <MoralisProvider appId={appId} serverUrl={serverUrl}>
      <AnonNews />
-    </MoralisProvider>
   )
 }
 
